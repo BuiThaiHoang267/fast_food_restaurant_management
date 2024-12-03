fb9ef36fbbbc62d9c91324cebd57ee0ab48fecb9
Bui Thai Hoang <buithaihoang04gl@gmail.com>
2024-12-03 23:04:35 +0700

    Add card ui

--- src/pages/report-sale-page.tsx
+++ src/pages/report-sale-page.tsx
@@ -1,15 +1,26 @@
-﻿import {Typography} from "@mui/material";
+﻿import {Divider, Typography} from "@mui/material";
 import {RadioBoxCard} from "../components/card.tsx";
 import {useEffect, useState} from "react";
 import CardTimeOrder from "../components/card-time-order.tsx";
 import dayjs, {Dayjs} from "dayjs";
 import {BarChart} from "@mui/x-charts";
-import {bg_blue_500, bg_green_500, bg_yellow_500} from "../common/constant.ts";
+import {
+    bg_blue_500,
+    bg_blue_600,
+    bg_green_500,
+    bg_grey_500,
+    bg_yellow_500, color_green_primary,
+    Error500,
+    success_500
+} from "../common/constant.ts";
 import {timeConverter} from "../utils/TimeElapsedConverter.ts";
 import {BranchService} from "../services/BranchService.ts";
 import {StatisticService} from "../services/StatisticService.ts";
 import {StatisticSaleDTO} from "../dtos/StatisticSaleDTO.ts";
 import {AxisDTO} from "../dtos/ResultSaleTodayDTO.ts";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import MovingIcon from "@mui/icons-material/Moving";
+import DescriptionIcon from "@mui/icons-material/Description";
 
 const ReportSalePage = () => {
     const reportOptions = [
@@ -90,19 +101,135 @@ const ReportSalePage = () => {
                 <CardTimeOrder onChangeDate={(start, end) => handleChangeDateInReportFilter(start, end)}/>
             </div>
             <div className="flex-grow flex-col pl-5 pr-10 py-5 space-y-2">
+
+                <div className={'flex flex-col p-4'}
+                     style=
+                         {{
+                             marginTop: "48px",
+                             backgroundColor: "white",
+                             borderRadius: "4px",
+                             boxShadow: "0 0 8px 0 rgba(0,0,0,0.1)",
+                         }}>
+                    <span style={{fontSize: "1rem", fontWeight: "bold"}}>THỐNG KÊ TỔNG QUAN</span>
+                    <div
+                        className={'flex flex-row flex-1 items-center pt-2'}
+                    >
+                        <div className={'flex flex-row flex-1 items-center'}>
+                            <div
+                                className={'flex flex-row items-center justify-center'}
+                                style={{height: 36, width: 36, borderRadius: 36, backgroundColor: bg_blue_600}}
+                            >
+                                <AttachMoneyIcon fontSize="medium" fontWeight='bold' sx={{color: 'white'}}/>
+                            </div>
+                            <div className={'flex-1 flex flex-col pl-4'}>
+                                <span
+                                    style={{fontSize: "0.9rem", fontWeight: "bold", color: bg_grey_500}}>Doanh số</span>
+                                <div className={'flex flex-row items-center'}>
+                                    <div style={{
+                                        fontSize: "1.3rem",
+                                        color: bg_blue_600
+                                    }}>{data.totalRevenue.toLocaleString()}</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Divider
+                            orientation="vertical"
+                            flexItem
+                            sx={{margin: '0 16px'}}
+                        />
+
+                        <div className={'flex flex-row flex-1 items-center'}>
+                            <div
+                                className={'flex flex-row items-center justify-center'}
+                                style={{height: 36, width: 36, borderRadius: 36, backgroundColor: color_green_primary}}
+                            >
+                                <MovingIcon fontSize="medium" fontWeight='bold' sx={{color: 'white'}}/>
+                            </div>
+                            <div className={'flex-1 flex flex-col pl-4'}>
+                                <span style={{
+                                    fontSize: "0.9rem",
+                                    fontWeight: "bold",
+                                    color: bg_grey_500
+                                }}>Lợi nhuận</span>
+                                <div className={'flex flex-row items-center'}>
+                                    <div style={{
+                                        fontSize: "1.3rem",
+                                        color: color_green_primary
+                                    }}>{data.totalProfit.toLocaleString()}</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Divider
+                            orientation="vertical"
+                            flexItem
+                            sx={{margin: '0 16px'}}
+                        />
+
+                        <div className={'flex flex-row flex-1 items-center'}>
+                            <div
+                                className={'flex flex-row items-center justify-center'}
+                                style={{height: 36, width: 36, borderRadius: 36, backgroundColor: 'orange'}}
+                            >
+                                <MovingIcon fontSize="medium" fontWeight='bold' sx={{color: 'white'}}/>
+                            </div>
+                            <div className={'flex-1 flex flex-col pl-4'}>
+                                <span style={{
+                                    fontSize: "0.9rem",
+                                    fontWeight: "bold",
+                                    color: bg_grey_500
+                                }}>Lợi nhuận</span>
+                                <div className={'flex flex-row items-center'}>
+                                    <div style={{
+                                        fontSize: "1.3rem",
+                                        color: 'orange'
+                                    }}>{data.totalProfit.toLocaleString()}</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Divider
+                            orientation="vertical"
+                            flexItem
+                            sx={{margin: '0 16px'}}
+                        />
+
+                        <div className={'flex flex-row flex-1 items-center'}>
+                            <div
+                                className={'flex flex-row items-center justify-center'}
+                                style={{height: 36, width: 36, borderRadius: 36, backgroundColor: Error500}}
+                            >
+                                <DescriptionIcon fontSize="small" fontWeight='bold' sx={{color: 'white'}}/>
+                            </div>
+                            <div className={'flex-1 flex flex-col pl-4'}>
+                                <span
+                                    style={{fontSize: "0.9rem", fontWeight: "bold", color: bg_grey_500}}>Hóa đơn</span>
+                                <div className={'flex flex-row items-center'}>
+                                    <div style={{fontSize: "1.3rem", color: Error500}}>{data.totalOrder}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
                 <div
                     className={"flex flex-col"}
                     style=
                         {{
                             marginTop: "16px",
                             backgroundColor: "white",
-                            borderRadius: "8px",
+                            borderRadius: "4px",
                             padding: "16px",
                             boxShadow: "0 0 8px 0 rgba(0,0,0,0.1)",
                             alignItems: "center"
                         }}
                 >
-                    <span style={{fontSize: "1rem", fontWeight: "normal"}}>Doanh Số {timeConverter(startDate.format("DD/MM/YYYY"), endDate.format("DD/MM/YYYY"),true)}</span>
+                    <span style={{
+                        fontSize: "1rem",
+                        fontWeight: "normal"
+                    }}>Doanh Số {timeConverter(startDate.format("DD/MM/YYYY"), endDate.format("DD/MM/YYYY"), true)}</span>
                     {reportOption === "time" &&
                         <BarChart
                             series={[
