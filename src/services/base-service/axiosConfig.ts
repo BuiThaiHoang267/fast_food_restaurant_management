21bf49449a1c2cfc27e803101ff032122464c158
Bui Thai Hoang <buithaihoang04gl@gmail.com>
2024-12-19 15:29:31 +0700

    doi url api

--- src/services/base-service/axiosConfig.ts
+++ src/services/base-service/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://52.221.203.133:8080/api',
+    baseURL: 'https://api.h2foodie.store/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
